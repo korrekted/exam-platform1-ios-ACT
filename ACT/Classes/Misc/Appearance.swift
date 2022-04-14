6147763c764133a77d4bdd0b9140679ca9408862
akonst17 <akonst17@gmail.com>
2022-04-14 18:32:34 +0300

    upgrade; build 9

--- ACT/Classes/Misc/Appearance.swift
+++ ACT/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 236, green: 73, blue: 64)
     static let progress2Color = UIColor(integralRed: 61, green: 148, blue: 245)
     static let progress3Color = UIColor(integralRed: 248, green: 207, blue: 86)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
